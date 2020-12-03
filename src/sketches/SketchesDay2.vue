afd533d309664f69b36f026e2e9d601a560ecaeb
Anthony Fu <anthonyfu117@hotmail.com>
2020-12-03 14:21:43 +0800

    (＠_＠)

--- src/sketches/SketchesDay2.vue
+++ src/sketches/SketchesDay2.vue
@@ -3,63 +3,71 @@
 </template>
 
 <script setup lang="ts">
-import P5 from 'p5'
-import { onMounted, ref } from 'vue'
+import { p5i } from 'p5i'
+import type { P5I } from 'p5i'
+import { onMounted, onUnmounted, ref } from 'vue'
 import { isDark } from '/~/logics'
 
 const el = ref<HTMLCanvasElement | null>(null)
 
-onMounted(() => {
-  const myp5 = new P5((s: P5) => {
-    s.setup = () => {
-      s.createCanvas(s.windowWidth / 2, s.windowHeight / 2)
-    }
+const {
+  mount, unmount,
+  createCanvas, resizeCanvas, background,
+  noFill, stroke, strokeJoin, strokeWeight, beginShape, endShape, vertex,
+  sin, cos, lerp, pow, millis,
+  PI, TWO_PI, BEVEL, CLOSE,
+} = p5i()
 
-    s.windowResized = () => {
-      s.resizeCanvas(s.windowWidth / 2, s.windowHeight / 2)
-    }
+function setup({ windowWidth, windowHeight }: P5I) {
+  createCanvas(windowWidth / 2, windowHeight / 2)
+}
 
-    s.draw = () => {
-      const dim = Math.min(s.width, s.height)
+function windowResized({ windowWidth, windowHeight }: P5I) {
+  resizeCanvas(windowWidth / 2, windowHeight / 2)
+}
 
-      // Black background
-      s.background(isDark.value ? '#222' : '#fff')
+function draw({ width, height }: P5I) {
+  const dim = Math.min(width, height)
 
-      // Stroke only with a specific join style and thickness
-      s.noFill()
-      s.stroke(isDark.value ? 255 : 0)
-      s.strokeJoin(s.BEVEL)
-      s.strokeWeight(dim * 0.015)
+  // Black background
+  background(isDark.value ? '#222' : '#fff')
 
-      // Get time in seconds
-      const time = s.millis() / 1000
+  // Stroke only with a specific join style and thickness
+  noFill()
+  stroke(isDark.value ? 255 : 0)
+  strokeJoin(BEVEL)
+  strokeWeight(dim * 0.015)
 
-      // Get a value that ping-pongs from 0 ... 1
-      const pingPong = s.sin(time * 0.75 - s.PI / 2) * 0.5 + 0.5
+  // Get time in seconds
+  const time = millis() / 1000
 
-      // Get a number of points, using pow() to skew to one end
-      const points = s.lerp(2, 100, s.pow(pingPong, 2.5))
+  // Get a value that ping-pongs from 0 ... 1
+  const pingPong = sin(time * 0.75 - PI / 2) * 0.5 + 0.5
 
-      // Size of shape
-      const radius = dim / 3
+  // Get a number of points, using pow() to skew to one end
+  const points = lerp(2, 100, pow(pingPong, 2.5))
 
-      // Draw shape with an angle offset
-      const angle = pingPong * s.PI * 2
-      polygon(s.width / 2, s.height / 2, radius, points, angle)
-    }
+  // Size of shape
+  const radius = dim / 3
 
-    function polygon(x: number, y: number, radius: number, sides = 3, angle = 0) {
-      s.beginShape()
-      for (let i = 0; i < sides; i++) {
-        const a = angle + s.TWO_PI * (i / sides)
-        let sx = x + s.cos(a) * radius
-        let sy = y + s.sin(a) * radius
-        s.vertex(sx, sy)
-      }
-      s.endShape(s.CLOSE)
-    }
-  }, el.value!)
-})
+  // Draw shape with an angle offset
+  const angle = pingPong * PI * 2
+  polygon(width / 2, height / 2, radius, points, angle)
+}
+
+function polygon(x: number, y: number, radius: number, sides = 3, angle = 0) {
+  beginShape()
+  for (let i = 0; i < sides; i++) {
+    const a = angle + TWO_PI * (i / sides)
+    const sx = x + cos(a) * radius
+    const sy = y + sin(a) * radius
+    vertex(sx, sy)
+  }
+  endShape(CLOSE)
+}
+
+onMounted(() => mount(el.value!, { setup, draw, windowResized }))
+onUnmounted(() => unmount())
 </script>
 
 <style scoped>

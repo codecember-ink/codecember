1666a3cbfc3912745083767e4310d55e64914f83
Pine Wu <octref@gmail.com>
2020-12-05 01:25:21 +0800

    (O.O)

---
+++ src/sketches/SketchesDay4.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="sketch" class="select-none">
+    <canvas ref="el" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref, watchEffect } from 'vue'
+import { isDark } from '/~/logics'
+
+const el = ref<HTMLCanvasElement | null>(null)
+
+onMounted(() => {
+  const canvas = el.value!
+  const context = canvas.getContext('2d')!
+
+  const size = 450
+  const dpr = window.devicePixelRatio || 1
+  canvas.style.width = `${size}px`
+  canvas.style.height = `${size}px`
+  canvas.width = size * dpr
+  canvas.height = size * dpr
+  context.scale(dpr, dpr)
+
+  const a = 1
+  const b = 3.14
+  const c = 0.33
+  const d = 2.8
+  const fill = 0.15
+
+  let x: number
+  let y: number
+  let i: number
+  let t: number
+  x = y = t = 0
+
+  canvas.onclick = () => {
+    x = y = t = 0
+    draw
+  }
+
+  watchEffect(() => {
+    context.fillStyle = isDark.value ? 'white' : 'black'
+    x = y = t = 0
+    draw()
+  })
+
+  function draw() {
+    context.clearRect(0, 0, size, size)
+    while (t < 300) {
+      for (i = 1e3; i--; ) {
+        context.fillRect((x * size) / 4 + size / 2, (y * size) / 4 + size / 2, fill, fill)
+        let xn = Math.sin(a * y) - Math.cos(b * x)
+        let yn = Math.sin(c * x) - Math.cos(d * y)
+        x = xn
+        y = yn
+      }
+      ++t
+    }
+  }
+  // canvas.onclick = draw
+
+  // function draw() {
+  //   context.clearRect(0, 0, canvas.width, canvas.height)
+
+  //   const step = 10
+  //   const lines = []
+
+  //   // Create the lines
+  //   for (let i = step; i <= size - step; i += step) {
+  //     const line = []
+  //     for (let j = step; j <= size - step; j += step) {
+  //       const distanceToCenter = Math.abs(j - size / 2)
+  //       const variance = Math.max(size / 2 - 50 - distanceToCenter, 0)
+  //       const random = ((Math.random() * variance) / 2) * -1
+  //       const point = { x: j, y: i + random }
+  //       line.push(point)
+  //     }
+  //     lines.push(line)
+  //   }
+
+  //   // Do the drawing
+  //   for (let i = 5; i < lines.length; i++) {
+  //     context.beginPath()
+  //     context.moveTo(lines[i][0].x, lines[i][0].y)
+
+  //     for (let j = 0; j < lines[i].length - 2; j++) {
+  //       const xc = (lines[i][j].x + lines[i][j + 1].x) / 2
+  //       const yc = (lines[i][j].y + lines[i][j + 1].y) / 2
+  //       context.quadraticCurveTo(lines[i][j].x, lines[i][j].y, xc, yc)
+  //     }
+
+  //     context.save()
+  //     context.globalCompositeOperation = 'destination-out'
+  //     context.fill()
+  //     context.restore()
+  //     context.stroke()
+  //   }
+  // }
+})
+</script>
+
+<style scoped>
+#sketch {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
